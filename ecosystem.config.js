860fbae881772db02cff584230a55f4b0533fe01
BenOnur <other.benonur@outlook.com>
2026-02-13 22:17:31 +0300

    PM2 interpreter venv python3 olarak duzeltildi

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
     apps: [{
         name: "twitch-miner",
         script: "./run.py",
-        interpreter: "python3",
+        interpreter: "./venv/bin/python3",
         autorestart: true,
         watch: false,
         max_memory_restart: "500M",
