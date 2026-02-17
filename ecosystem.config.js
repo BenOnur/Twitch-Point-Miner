06c1855a9c84b038f5c0425c1a842b415f82dfb8
BenOnur <other.benonur@outlook.com>
2026-02-17 04:52:44 +0300

    Fix syntax in ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,8 @@ module.exports = {
         interpreter: "./venv/bin/python3",
         autorestart: true,
         watch: false,
-        ignore_watch: ["node_modules", "logs", "analytics", "*.json"],
+        ignore_watch: ["node_modules", "logs", "analytics", "*.json", "*.log", "__pycache__", "venv"],
+        max_memory_restart: "1024M",
         exp_backoff_restart_delay: 100,
         env: {
             PYTHONUNBUFFERED: "1"
